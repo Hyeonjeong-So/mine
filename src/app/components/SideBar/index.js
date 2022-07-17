c403116fee6d805266d2e6dba666905233177b65
soy <soy@Garyui-MacBookPro.local>
2022-07-17 15:33:43 +0900

    scoring slider commit

--- src/app/components/SideBar/index.js
+++ src/app/components/SideBar/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { SideBarWrapper } from 'app/components/SideBar/styles';
-import Slider from 'app/components/SideBar/Slider';
+
+import ScoringSlider from 'app/components/SideBar/ScoringSlider';
 import Profile from 'app/components/SideBar/Profile';
 import Music from 'app/components/SideBar/Music';
 
 const SideBar = () => {
   return (
     <SideBarWrapper>
-      <Slider />
+      <ScoringSlider />
       <Profile />
       <Music />
     </SideBarWrapper>
