7d81c1681d7394aca9bade0a748834b2495781ce
soy <soy@Garyui-MacBookPro.local>
2022-07-17 16:13:46 +0900

    music commit

--- src/app/components/SideBar/Music/index.js
+++ src/app/components/SideBar/Music/index.js
@@ -1,7 +1,58 @@
 import React from 'react';
 
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+
 const Music = () => {
-  return <div>Music</div>;
+  const theme = useTheme();
+
+  return (
+    <Card
+      sx={{
+        display: 'flex',
+        marginTop: '30px',
+        width: '250px',
+        height: '150px',
+      }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="14">
+            Like Im Gonna Lose You
+          </Typography>
+          <Typography variant="10" color="text.secondary" component="div">
+            Meghan Trainor
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+          <IconButton aria-label="previous">
+            {theme.direction === 'rtl' ? (
+              <SkipNextIcon />
+            ) : (
+              <SkipPreviousIcon />
+            )}
+          </IconButton>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon sx={{ height: 20, width: 20 }} />
+          </IconButton>
+          <IconButton aria-label="next">
+            {theme.direction === 'rtl' ? (
+              <SkipPreviousIcon />
+            ) : (
+              <SkipNextIcon />
+            )}
+          </IconButton>
+        </Box>
+      </Box>
+    </Card>
+  );
 };
 
 export default Music;
