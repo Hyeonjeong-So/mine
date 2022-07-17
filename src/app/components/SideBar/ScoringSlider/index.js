07720c51e465baf43a7db6c24536015ff918dd5e
Soy <93440978+Hyeonjeong-So@users.noreply.github.com>
2022-07-17 15:41:02 +0900

    scoring slider commit (#3)
    * scoring slider commit
    
    * 개행 수정
    
    Co-authored-by: Itmom <53969314+Cho-Geonwoo@users.noreply.github.com>
    
    Co-authored-by: soy <soy@Garyui-MacBookPro.local>
    Co-authored-by: Itmom <53969314+Cho-Geonwoo@users.noreply.github.com>

---
+++ src/app/components/SideBar/ScoringSlider/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+const marks = [
+  {
+    value: 0,
+    label: '0',
+  },
+  {
+    value: 30,
+    label: '30',
+  },
+  {
+    value: 50,
+    label: '50',
+  },
+  {
+    value: 100,
+    label: '100',
+  },
+];
+
+function valuetext(value) {
+  return `${value}`;
+}
+
+const ScoringSlider = () => {
+  return (
+    <Box sx={{ width: 250, marginTop: 5, marginLeft: 1 }}>
+      <Slider
+        aria-label="Custom marks"
+        defaultValue={20}
+        getAriaValueText={valuetext}
+        step={5}
+        valueLabelDisplay="auto"
+        marks={marks}
+      />
+    </Box>
+  );
+};
+
+export default ScoringSlider;
