61299c7701d7a44685cdaef2fe26d76bc8b4c13a
baron <baron@hpcnt.com>
2022-07-17 15:01:12 +0900

    feat: sidebar components

--- src/app/components/SideBar/index.js
+++ src/app/components/SideBar/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+
 import { SideBarWrapper } from 'app/components/SideBar/styles';
+import Slider from 'app/components/SideBar/Slider';
+import Profile from 'app/components/SideBar/Profile';
+import Music from 'app/components/SideBar/Music';
 
 const SideBar = () => {
-  return <SideBarWrapper>sidebar</SideBarWrapper>;
+  return (
+    <SideBarWrapper>
+      <Slider />
+      <Profile />
+      <Music />
+    </SideBarWrapper>
+  );
 };
 
 export default SideBar;
